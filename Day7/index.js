7173d4f0b818b7261c04b1c56c9abdfde6989257
Parisheelan Negi <parisheelan@gmail.com>
2022-02-05 22:47:14 +0530

    Basic DOM manipulation on button elements

---
+++ Day7/index.js
@@ -0,0 +1,9 @@
+document.querySelector("button").addEventListener("click",function(){
+    document.querySelector("button").classList.add("press");
+    setTimeout(function(){
+        document.querySelector("button").classList.remove("press");
+    },100)
+})
+        
+    
+    
